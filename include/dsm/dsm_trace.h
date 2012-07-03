24ae7d27c6920f348edb8b57750ac7752f227517
Benoit <benoit.hudzia@sap.com>
2012-07-03 13:32:36 +0100

    refactoring trace v3

--- include/dsm/dsm_trace.h
+++ include/dsm/dsm_trace.h
@@ -12,7 +12,7 @@
 #include <linux/tracepoint.h>
 
 DECLARE_EVENT_CLASS(dsm_page_fault_template,
-        TP_PROTO( int dsm_id, int svm_id,int remote_dsm_id, int remote_svm_id, unsigned long address, int tag,int nb_request_for_page ),
+        TP_PROTO( int dsm_id, int svm_id,int remote_dsm_id, int remote_svm_id, unsigned long address, int tag),
         TP_ARGS( dsm_id, svm_id, remote_dsm_id, remote_svm_id, address, tag),
 
         TP_STRUCT__entry( __field(int, dsm_id ) __field(int, svm_id) __field(int, remote_dsm_id ) __field(int, remote_svm_id) __field(void *, page_addr) __field(int, tag) ),
@@ -22,23 +22,23 @@ DECLARE_EVENT_CLASS(dsm_page_fault_template,
         TP_printk(" %p | %d | %d | %d | %d | %d", __entry->page_addr, __entry->dsm_id, __entry->svm_id, __entry->remote_dsm_id, __entry->remote_svm_id, __entry->tag ));
 
 DEFINE_EVENT(dsm_page_fault_template, do_dsm_page_fault_svm,
-        TP_PROTO(int event, int dsm_id, int svm_id,int remote_dsm_id, int remote_svm_id, unsigned long address, int tag,int nb_request_for_page ),
+        TP_PROTO(int event, int dsm_id, int svm_id,int remote_dsm_id, int remote_svm_id, unsigned long address, int tag),
         TP_ARGS(event, dsm_id, svm_id, remote_dsm_id, remote_svm_id, address, tag));
 
 DEFINE_EVENT(dsm_page_fault_template, do_dsm_page_fault_svm_complete,
-        TP_PROTO( int dsm_id, int svm_id,int remote_dsm_id, int remote_svm_id, unsigned long address, int tag,int nb_request_for_page ),
+        TP_PROTO( int dsm_id, int svm_id,int remote_dsm_id, int remote_svm_id, unsigned long address, int tag ),
         TP_ARGS( dsm_id, svm_id, remote_dsm_id, remote_svm_id, address, tag));
 
 DEFINE_EVENT(dsm_page_fault_template, dsm_get_remote_page,
-        TP_PROTO( int dsm_id, int svm_id,int remote_dsm_id, int remote_svm_id, unsigned long address, int tag,int nb_request_for_page ),
+        TP_PROTO( int dsm_id, int svm_id,int remote_dsm_id, int remote_svm_id, unsigned long address, int tag ),
         TP_ARGS( dsm_id, svm_id, remote_dsm_id, remote_svm_id, address, tag));
 
 DEFINE_EVENT(dsm_page_fault_template, dsm_pull_req_complete,
-        TP_PROTO( int dsm_id, int svm_id,int remote_dsm_id, int remote_svm_id, unsigned long address, int tag,int nb_request_for_page ),
+        TP_PROTO( int dsm_id, int svm_id,int remote_dsm_id, int remote_svm_id, unsigned long address, int tag ),
         TP_ARGS( dsm_id, svm_id, remote_dsm_id, remote_svm_id, address, tag));
 
 DEFINE_EVENT(dsm_page_fault_template, dsm_try_pull_req_complete_fail,
-        TP_PROTO( int dsm_id, int svm_id,int remote_dsm_id, int remote_svm_id, unsigned long address, int tag,int nb_request_for_page ),
+        TP_PROTO( int dsm_id, int svm_id,int remote_dsm_id, int remote_svm_id, unsigned long address, int tag ),
         TP_ARGS( dsm_id, svm_id, remote_dsm_id, remote_svm_id, address, tag));
 
 #endif

a70a8dec7197a74eb9605d10e6ac39411dd759f2
Benoit <benoit.hudzia@sap.com>
2012-07-25 14:50:31 +0100

    better tracing v2

--- drivers/dsm/dsm_handlers.c
+++ drivers/dsm/dsm_handlers.c
@@ -120,14 +120,13 @@ static inline int flush_dsm_request_queue(struct conn_element *ele) {
     struct tx_buf_ele *tx_e = NULL;
     int ret =0;
 
-
+    atomic_set(&ele->schedule_flush,0);
     mutex_lock(&tx->flush_mutex);
     head = llist_del_all(&tx->request_queue);
-    atomic_set(&ele->schedule_flush,0);
     add_to_ordered_queue(head, ele);
 
     while (!list_empty(&tx->ordered_request_queue)) {
-        trace_flushing_requests(0,0,0,0,0,0);
+        trace_flushing_requests(tx->request_queue_sz,0,0,0,0,0);
         tx_e = try_get_next_empty_tx_ele(ele);
         if (!tx_e) {
             ret = 1;
@@ -135,7 +134,7 @@ static inline int flush_dsm_request_queue(struct conn_element *ele) {
         }
         req= list_first_entry(&tx->ordered_request_queue, struct dsm_request, ordered_list);
 
-        trace_flushing_requests(0,0,0,0,req->addr,0);
+        trace_flushing_requests(tx->request_queue_sz,0,0,0,req->addr,0);
         process_dsm_request(ele, req, tx_e);
         list_del(&req->ordered_list);
         release_dsm_request(req);

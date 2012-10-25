2f95c7d8f814524934d46f83ed538bea1a7d6e38
Steve Walsh <steve.walsh@sap.com>
2012-10-25 11:36:43 +0100

    Fixed release function so that all svms are released before releasing dsm

--- drivers/dsm/dsm.c
+++ drivers/dsm/dsm.c
@@ -202,27 +202,16 @@ static int release(struct inode *inode, struct file *f)
 {
     struct private_data *data = (struct private_data *) f->private_data;
     struct dsm *dsm = data->dsm;
-    struct list_head *pos = NULL;
-    u32 remove, svm_id;
+    struct subvirtual_machine *svm = NULL;
 
     if (!dsm)
         return 1;
 
-    rcu_read_lock();
-    remove = 0;
-    list_for_each (pos, &dsm->svm_list) {
-        struct subvirtual_machine *svm = list_entry(pos,
-                struct subvirtual_machine, svm_ptr);
-        if (svm->mm == current->mm) {
-            svm_id = svm->svm_id;
-            remove = 1;
-            break;
-        }
+    while ( !list_empty(&dsm->svm_list) ) {
+        svm = list_first_entry(&dsm->svm_list, struct subvirtual_machine, svm_ptr);
+        dsm_printk("removing svm_id: %d from list of svms", svm->svm_id);
+        remove_svm(dsm->dsm_id, svm->svm_id);
     }
-    rcu_read_unlock();
-
-    if (remove)
-        remove_svm(dsm->dsm_id, svm_id);
 
     if (data->dsm->nb_local_svm == 0) {
         remove_dsm(data->dsm);

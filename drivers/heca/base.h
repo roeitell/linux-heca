77c8d45b11ea61b3c8a386cc68d46715c542c2bf
Benoit Hudzia <benoit.hudzia@gmail.com>
2013-08-11 12:58:53 -0700

    [trivial] Removing tabs, minor formatting cleanup , enforcing 8 space indenttion and 80 carachters line ( almost everywhere )  width in base.h

--- drivers/heca/base.h
+++ drivers/heca/base.h
@@ -22,7 +22,7 @@ int create_dsm(__u32 dsm_id);
 /* svm */
 inline struct subvirtual_machine *find_svm(struct dsm *, u32);
 inline struct subvirtual_machine *find_local_svm_in_dsm(struct dsm *,
-        struct mm_struct *);
+                struct mm_struct *);
 inline struct subvirtual_machine *find_local_svm_from_mm(struct mm_struct *mm);
 int create_svm(struct hecaioc_svm *svm_info);
 inline void release_svm(struct subvirtual_machine *);
@@ -32,7 +32,7 @@ struct subvirtual_machine *find_any_svm(struct dsm *, struct svm_list);
 /* mr */
 struct memory_region *find_mr(struct subvirtual_machine *, u32);
 struct memory_region *search_mr_by_addr(struct subvirtual_machine *,
-        unsigned long);
+                unsigned long);
 int create_mr(struct hecaioc_mr *udata);
 
 /* ps */
@@ -41,7 +41,7 @@ int unmap_ps(struct hecaioc_ps *udata);
 
 /* rcm */
 int create_rcm_listener(struct dsm_module_state *, unsigned long,
-        unsigned short);
+                unsigned short);
 int destroy_rcm_listener(struct dsm_module_state *);
 int init_rcm(void);
 int fini_rcm(void);

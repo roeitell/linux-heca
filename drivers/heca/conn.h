136e2914dcbef2d33eb6831b5d6d1dcdf0b13f8a
Benoit <benoit.hudzia@sap.com>
2013-08-12 11:34:17 +0100

    removed unecesary variable name in funtion definition in conn.h

--- drivers/heca/conn.h
+++ drivers/heca/conn.h
@@ -14,19 +14,18 @@ void init_kmem_request_cache(void);
 void destroy_kmem_request_cache(void);
 inline struct dsm_request *alloc_dsm_request(void);
 inline void release_dsm_request(struct dsm_request *);
-int add_dsm_request(struct dsm_request *, struct conn_element *, u16,
-                u32, u32, u32, u32, unsigned long, int (*)(struct tx_buf_ele *),
-                struct dsm_page_cache *, struct page *, struct page_pool_ele *, int,
-                struct dsm_message *);
+int add_dsm_request(struct dsm_request *, struct conn_element *, u16, u32, u32,
+                u32, u32, unsigned long, int (*)(struct tx_buf_ele *),
+                struct dsm_page_cache *, struct page *, struct page_pool_ele *,
+                int, struct dsm_message *);
 inline int request_queue_empty(struct conn_element *);
 inline int request_queue_full(struct conn_element *);
 void dsm_request_queue_merge(struct tx_buffer *);
-void create_page_reclaim_request(struct tx_buf_ele *tx_e, u32 dsm_id, u32
-                mr_id, u32 local_id, u32 remote_id, uint64_t addr);
-void create_page_request(struct conn_element *ele, struct tx_buf_ele *tx_e,
-                u32 dsm_id, u32 mr_id, u32 local_id, u32 remote_id,
-                uint64_t addr, struct page *page, struct dsm_page_cache *dpc,
-                struct page_pool_ele *ppe);
+void create_page_reclaim_request(struct tx_buf_ele *, u32, u32
+                , u32, u32, uint64_t);
+void create_page_request(struct conn_element *, struct tx_buf_ele *, u32, u32,
+                u32, u32, uint64_t, struct page *, struct dsm_page_cache *,
+                struct page_pool_ele *);
 void create_page_pull_request(struct conn_element *, struct tx_buf_ele *,
                 u32, u32, u32, u32, uint64_t);
 void listener_cq_handle(struct ib_cq *, void *);
@@ -36,19 +35,19 @@ void release_tx_element(struct conn_element *, struct tx_buf_ele *);
 void release_tx_element_reply(struct conn_element *, struct tx_buf_ele *);
 void try_release_tx_element(struct conn_element *, struct tx_buf_ele *);
 int connect_svm(__u32, __u32, unsigned long, unsigned short);
-unsigned long inet_addr(const char *cp);
-char *inet_ntoa(unsigned long s_addr, char *buf, int sz);
+unsigned long inet_addr(const char *);
+char *inet_ntoa(unsigned long, char *, int);
 struct tx_buf_ele *try_get_next_empty_tx_ele(struct conn_element *, int);
 struct tx_buf_ele *try_get_next_empty_tx_reply_ele(struct conn_element *);
 int destroy_connection(struct conn_element *);
 int tx_dsm_send(struct conn_element *, struct tx_buf_ele *);
-char *port_ntoa(unsigned short port, char *buf, int sz);
-char *sockaddr_ntoa(struct sockaddr_in *sa, char *buf, int sz);
-char *conn_ntoa(struct sockaddr_in *src, struct sockaddr_in *dst, char *buf,
-                int sz);
+char *port_ntoa(unsigned short, char *, int);
+char *sockaddr_ntoa(struct sockaddr_in *, char *, int);
+char *conn_ntoa(struct sockaddr_in *, struct sockaddr_in *, char *, int);
 int dsm_send_tx_e(struct conn_element *, struct tx_buf_ele *, int, int, u32,
                 u32, u32, u32, unsigned long, unsigned long,
-                struct dsm_page_cache *, struct page *, struct page_pool_ele *, int,
-                int (*)(struct tx_buf_ele *), struct dsm_message *);
+                struct dsm_page_cache *, struct page *,
+                struct page_pool_ele *, int, int (*)(struct tx_buf_ele *),
+                struct dsm_message *);
 
 #endif /* _HECA_CONN_H */
